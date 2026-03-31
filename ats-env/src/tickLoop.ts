7ce81bf6060d1fa304c598b79482e085ba547ee7
elam <belamparithi22@gmail.com>
2026-03-31 15:13:24 +0200

    Fixed the tick frequency issue and docker compose build issue for latest image

--- ats-env/src/tickLoop.ts
+++ ats-env/src/tickLoop.ts
@@ -6,7 +6,7 @@ import { integrate } from "./physics.js";
 import { publishObservation, publishSnapshot } from "./natsClient.js";
 
 const TICK_MS = 50; // 20 Hz
-const CMD_STALE_MS = 100;
+const CMD_STALE_MS = 250;
 const DEFAULT_SPEED_LIMIT = 14; // m/s
 const SAFE_CMD = { desired_accel: -2, desired_steer: 0 } as const;
 
